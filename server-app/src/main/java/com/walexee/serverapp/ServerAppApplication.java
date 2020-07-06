39652bf440532c018788f93d14d0a0c57930b061
Olawale Adeleye <olawale.adeleye@hotmail.com>
2020-07-06 18:08:32 -0400

    initial commit

---
+++ server-app/src/main/java/com/walexee/serverapp/ServerAppApplication.java
@@ -0,0 +1,15 @@
+package com.walexee.serverapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ServerAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerAppApplication.class, args);
+    }
+
+}
---
+++ server-app/src/test/java/com/walexee/serverapp/ServerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walexee.serverapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

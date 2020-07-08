f39a3e43f59b288aaec51062004c2a559941aebb
Olawale Adeleye <olawale.adeleye@hotmail.com>
2020-07-08 18:12:34 -0400

    added refreshScope annoation to appSettings

--- client-app/src/main/java/com/walexee/clientapp/AppSettings.java
+++ client-app/src/main/java/com/walexee/clientapp/AppSettings.java
@@ -2,10 +2,12 @@ package com.walexee.clientapp;
 
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Service;
 
 @Service
 @Data
+@RefreshScope // needed to refresh this bean when a refresh is triggered from the RESTful endpoint
 public class AppSettings {
     @Value("${rate}")
     private String rate;
